ad3e590a144235c254ed68492fbd0d44a6640479
kostet72 <kostagarifullin275@gmail.com>
2023-07-05 14:24:26 +0500

    add function for counting correct anserw(points) the 'Question' and 'Answer' classes are described

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+''' ОПИСАНИЕ КЛАССОВ ДЛЯ ВОПРОСА И ОТВЕТА
+class Question(models.Model):
+    text=models.CharField(max_length=255)
+
+class Answer(models.Model):
+    question=models.ForeignKey(Question, on_delete=models.CASCADE)
+    text=models.CharField(max_length=255)
+    is_correct=models.BooleanField(default=False)
+'''
--- tests/views.py
+++ tests/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from models import Question, Answer почему-то не хочет их подгружать
 
 def tests_home(request):
     return render(request, 'tests/tests_home.html')
+
+''' ФУНКЦИЯ ДЛЯ ПОДСЧЁТА ОЧКОВ ЗА ПРАВИЛЬНЫЕ ОТВЕТЫ С ПЕРЕХОДОМ НА СТРАНИЦУ СО СЧЁТОМ И ОБРАТНО НА ВОПРОС
+def test_view_result(request):
+    if request.method == "POST":
+        score=0
+        your_answer=request.POST.getlist(answer)
+        for answer_id in your_answer:
+            answer=Answer.objects.get(is=answer_id)
+            if answer.is_correct:
+                score+=1
+        return render(request,'score',{'':score}) # в скобках указать html файл для страницы с кол-вом баллов
+    question=Question.objects.all()
+    return render(request,'question',{'':question}) # в скобках указать html файл для страницы с вопросом
+'''
