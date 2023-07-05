c0e54a20f5c817f9e469764645d396288dde6d54
Abotigen-56 <nenashev.2004@bk.ru>
2023-07-05 18:59:33 +0500

    logout

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import LogoutUser
 
 urlpatterns = [
     path('homepage/', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('logout/', LogoutUser, name='logout')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+
 
 def home(request):
     return render(request, 'main/homepage.html')
 
 def about(request):
     return render(request, 'main/about.html')
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('/')
