a1bab9454ecf5154f1aeda6bfb7cbcfc7fc82a0e
Abotigen-56 <nenashev.2004@bk.ru>
2023-07-09 16:27:02 +0500

    fix birthday_date in profile

--- users/models.py
+++ users/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     surname = models.CharField('Отчество', max_length=25, default='')
+    birthday_date = models.DateField('День рождения', null=True)
